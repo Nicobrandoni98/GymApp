bf4c3c848d0c62326417fee591799ad41506d3cf
Nicobrandoni98 <nico.brandoni98@gmail.com>
2024-09-18 17:04:31 -0300

    Accediendo a la informacion desde el backend

--- backend/index.js
+++ backend/index.js
@@ -1,61 +1,73 @@
-const express = require('express')
-const app = express()
-const requestLogger = require('./middleware/requestLogger')
-const unknownEndpoint = require('./middleware/unknownEndpoint')
-app.use(express.json())
-
-app.use(requestLogger)
-
+const express = require("express");
+const app = express();
+const requestLogger = require("./middleware/requestLogger.js");
+const unknownEndpoint = require("./middleware/unknownEndpoint");
+const cors = require("cors");
+app.use(express.json());
+app.use(cors());
+app.use(requestLogger);
 
 let cardData = [
   {
     id: "1",
-    title: "Pecho",
-    text: "Ejercicios para pecho",
-    buttonText: "Ir a los ejercicios",
+    title: "Ejercicios para pecho",
+    exercises: [
+      "Press Banca",
+      "Press banca inclinado",
+      "Apertura con mancuernas",
+      "Cruce de Poleas",
+    ],
     img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRu8qK5mNV_W1j823KeA_qfF8wFLdoihyIsaA&s",
   },
   {
     id: "2",
-    title: "Espalda",
-    text: "Ejercicios para espalda",
-    buttonText: "Ir a lo ejercicios",
+    title: "Ejercicios para espalda",
+    exercises: [
+      "Jalon al pecho",
+      "Remo con barra",
+      "Remo T",
+      "Remo con mancuernas",
+    ],
     img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ5RZALev86EkvtZhn9ejHXhnnpw6UEA9-Pog&s",
   },
   {
     id: "3",
-    title: "Piernas",
-    text: "Ejercicios para piernas",
-    buttonText: "Ir a los ejercicios",
+    title: "Ejercicios para piernas",
+    exercises: [
+      "Sentadilla",
+      "Peso muerto",
+      "Extension de cuadriceps",
+      "Prensa",
+    ],
     img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSiHK-P8nRBBKNynjNxL295MUtkLDa5FNfKpA&s",
   },
   {
     id: "4",
-    title: "Brazos",
-    text: "Ejercicios para brazos",
-    buttonText: "Ir a los ejercicios",
+    title: "Ejercicios para brazos",
+    exercises: [
+      "Curl de bicep",
+      "Extension triceps con soga",
+      "Vuelos laterales",
+    ],
     img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR3XyBlGIc8hO8R1BRwCjZrDlkNQcgZ42E02u08_L3kKQxZ1GE9nN1LQT26O9sZO-lHugI&usqp=CAU",
   },
 ];
 
-app.get('/', (request, response) => {
-    response.send('Hellow')
-})
+app.get("/", (request, response) => {
+  response.send("Hellow");
+});
 
-app.get('/api/categories', (request, response) => {
-    response.json(cardData)
-})
+app.get("/api/categories", (request, response) => {
+  response.json(cardData);
+});
 
-app.get('/api/categories/:id', (request, response) => {
-    const id = request.params.id
-    const category = cardData.find(category => {
-      return category.id === id
-    })
-    response.json(category)
-  })
-  
-  
-app.use(unknownEndpoint)
+app.get("/api/categories/:id", (request, response) => {
+  const id = request.params.id;
+  const category = cardData.find((category) => category.id === id);
+  response.json(category);
+});
+
+app.use(unknownEndpoint);
 
 const PORT = 3001;
 app.listen(PORT);
